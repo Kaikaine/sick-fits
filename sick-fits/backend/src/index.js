e638a32c42af095bf267dc3f1afe34a1e9319568
Kaikaine <kairijb@protonmail.com>
2019-12-10 21:17:08 -0500

    mutations and queries

--- sick-fits/backend/src/index.js
+++ sick-fits/backend/src/index.js
@@ -1,1 +1,20 @@
-// let's go!
+require("dotenv").config({ path: "variables.env" });
+const createServer = require("./createServer");
+const db = require("./db");
+
+const server = createServer();
+
+// Use express middleware to handle cookies (jwt)
+// Use express middleware to populate current user
+
+server.start(
+  {
+    cors: {
+      credentials: true,
+      origin: process.env.FRONTEND_URL
+    }
+  },
+  details => {
+    console.log(`Server running on port ${details.port}`);
+  }
+);
